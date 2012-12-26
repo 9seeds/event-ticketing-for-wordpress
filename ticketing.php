d691575885537056f15f393bbf495c23df6a22cf
Ben Lobaugh <ben@lobaugh.net>
2012-12-25 17:38:25 -0800

    Adding some basic plugin code

--- ticketing.php
+++ ticketing.php
@@ -8,3 +8,18 @@ Version: 2.0
 Author URI: http://9seeds.com/
 Text Domain: wpevt
 */
+
+define( 'WPEVT_PLUGIN_DIR', trailingslashit( dirname( __FILE__) ) );
+define( 'WPEVT_PLUGIN_URL', trailingslashit( WP_CONTENT_URL . '/' . basename( __DIR__ ))  );
+
+
+require_once( 'lib/WPEVT.class.php' );
+
+register_activation_hook( __FILE__, array( 'WPEVT', 'activate' ) );
+
+register_deactivation_hook( __FILE__, array( 'WPEVT', 'deactivate' ) );
+
+register_uninstall_hook( __FILE__, array( 'WPEVT', 'uninstall' ) );
+
+$wpevt = new WPEVT();
+
