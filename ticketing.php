4e7b91c7a9dca54ad51e1987d3f086aca699a3b4
Ben Lobaugh <ben@lobaugh.net>
2012-12-25 17:09:59 -0800

    Adding initial base structure

---
+++ ticketing.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: WP Event Ticketing
+Plugin URI: http://9seeds.com/plugins/
+Description: The WP Event Ticketing plugin makes it easy to sell and manage tickets for your event.
+Author: 9seeds.com
+Version: 2.0
+Author URI: http://9seeds.com/
+Text Domain: wpevt
+*/
