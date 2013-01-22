b3950ef5bc4fe001355139bce1b2e19ac8cab8c5
John Hawkins <john@9seeds.com>
2013-01-21 18:40:48 -0800

    holding for a pull
    holding for a pull
    
    Signed-off-by: John Hawkins <john@9seeds.com>

--- views/admin/reporting.php
+++ views/admin/reporting.php
@@ -1,7 +1,38 @@
-reporting<?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+<div class="wrap">
+	<a href="http://9seeds.com/" target="_blank"><div id="9seeds-icon" style="background: url('/wp-content/plugins/wp-event-ticketing/images/9seeds-logo.png') no-repeat; height: 32px; width: 32px; float: left; margin: 7px 5px 0 0;"></div></a>
+	<h2><?php _e( 'Reporting', 'wpet' ); ?></h2>
+	<div id="9seeds_content_top" class="postbox-container" style="">
+		<div class="metabox-holder" style="width: 70%; border: solid 1px; float: left;">
+			<div class="meta-box-sortables ui-sortable" style="">
+				<div class="event-at-a-glance" style="width: 45%; margin-right: 20px; float: left;">
+					<div class="single-row" style="width: 70%; float: left;">
+						<div class="left-box" style="border: solid 1px; padding: 5px; float: left;">
+							<div><label><?php _e( 'Event', 'wpet' ); ?>:</label> My Event Name</div>
+							<div><label><?php _e( 'Date', 'wpet' ); ?>:</label> July 6, 2014</div>
+							<div><label><?php _e( 'Total tickets', 'wpet' ); ?>:</label> 250</div>
+							<div><label><?php _e( 'Tickets go on sale', 'wpet' ); ?>:</label> April 1, 2014</div>
+							<div><label><?php _e( 'Ticket sales end on', 'wpet' ); ?>:</label> July 5, 2014</div>
+						</div>
+						<div class="event-at-a-glance" style="width: 45%; float: left;">
+							<div class="right-box" style="border: solid 1px; padding: 5px;">
+								<div><label><?php _e( 'Event', 'wpet' ); ?>:</label> My Event Name</div>
+								<div><label><?php _e( 'Date', 'wpet' ); ?>:</label> July 6, 2014</div>
+								<div><label><?php _e( 'Total tickets', 'wpet' ); ?>:</label> 250</div>
+								<div><label><?php _e( 'Tickets go on sale', 'wpet' ); ?>:</label> April 1, 2014</div>
+								<div><label><?php _e( 'Ticket sales end on', 'wpet' ); ?>:</label> July 5, 2014</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="9seeds_sidebar" class="postbox-container" style="width: 30%;">
+			<div id="sidebar">
+				<div id="" class="box-type-1">
+					<h2>Upgrade to Pro</h2>
+					<p>Some random info here.</p>	
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
