bd5d5cf0ef274c46a9a202be08c314ad7b10ff2d
John Hawkins <john@9seeds.com>
2013-01-22 10:42:40 -0800

    hold for pull
    hold for pull
    
    Signed-off-by: John Hawkins <john@9seeds.com>

--- views/admin/reporting.php
+++ views/admin/reporting.php
@@ -1,38 +1,32 @@
 <div class="wrap">
 	<a href="http://9seeds.com/" target="_blank"><div id="seeds-icon"></div></a>
 	<h2><?php _e( 'Reporting', 'wpet' ); ?></h2>
-	<div id="9seeds_content_top" class="postbox-container" style="">
-		<div class="metabox-holder" style="width: 70%; border: solid 1px; float: left;">
-			<div class="meta-box-sortables ui-sortable" style="">
-				<div class="event-at-a-glance" style="width: 45%; margin-right: 20px; float: left;">
-					<div class="single-row" style="width: 70%; float: left;">
-						<div class="left-box" style="border: solid 1px; padding: 5px; float: left;">
-							<div><label><?php _e( 'Event', 'wpet' ); ?>:</label> My Event Name</div>
-							<div><label><?php _e( 'Date', 'wpet' ); ?>:</label> July 6, 2014</div>
-							<div><label><?php _e( 'Total tickets', 'wpet' ); ?>:</label> 250</div>
-							<div><label><?php _e( 'Tickets go on sale', 'wpet' ); ?>:</label> April 1, 2014</div>
-							<div><label><?php _e( 'Ticket sales end on', 'wpet' ); ?>:</label> July 5, 2014</div>
-						</div>
-						<div class="event-at-a-glance" style="width: 45%; float: left;">
-							<div class="right-box" style="border: solid 1px; padding: 5px;">
-								<div><label><?php _e( 'Event', 'wpet' ); ?>:</label> My Event Name</div>
-								<div><label><?php _e( 'Date', 'wpet' ); ?>:</label> July 6, 2014</div>
-								<div><label><?php _e( 'Total tickets', 'wpet' ); ?>:</label> 250</div>
-								<div><label><?php _e( 'Tickets go on sale', 'wpet' ); ?>:</label> April 1, 2014</div>
-								<div><label><?php _e( 'Ticket sales end on', 'wpet' ); ?>:</label> July 5, 2014</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+	
+	<div class="seeds-admin-left">
+		<div class="report-left">
+			<div><label><?php _e( 'Event', 'wpet' ); ?>:</label> My Event Name</div>
+			<div><label><?php _e( 'Date', 'wpet' ); ?>:</label> July 6, 2014</div>
+			<div><label><?php _e( 'Total tickets', 'wpet' ); ?>:</label> 250</div>
+			<div><label><?php _e( 'Tickets go on sale', 'wpet' ); ?>:</label> April 1, 2014</div>
+			<div><label><?php _e( 'Ticket sales end on', 'wpet' ); ?>:</label> July 5, 2014</div>
 		</div>
-		<div id="9seeds_sidebar" class="postbox-container" style="width: 30%;">
-			<div id="sidebar">
-				<div id="" class="box-type-1">
-					<h2>Upgrade to Pro</h2>
-					<p>Some random info here.</p>	
-				</div>
-			</div>
+		<div class="report-right">
+			<div><label><?php _e( 'Event', 'wpet' ); ?>:</label> My Event Name</div>
+			<div><label><?php _e( 'Date', 'wpet' ); ?>:</label> July 6, 2014</div>
+			<div><label><?php _e( 'Total tickets', 'wpet' ); ?>:</label> 250</div>
+			<div><label><?php _e( 'Tickets go on sale', 'wpet' ); ?>:</label> April 1, 2014</div>
+			<div><label><?php _e( 'Ticket sales end on', 'wpet' ); ?>:</label> July 5, 2014</div>
 		</div>
 	</div>
+	
+	<div class="seeds-admin-right">
+		<div class="seeds-sidebar-box">
+			<h2>Upgrade to Pro</h2>
+			<p>Some random info here.</p>	
+		</div>
+		<div class="seeds-sidebar-box-2">
+			<h2>Upgrade to Pro</h2>
+			<p>Some random info here.</p>	
+		</div>
+	</div>	
 </div>
