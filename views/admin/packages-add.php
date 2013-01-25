649a77bec26105b90bd0f43ba01c9ceef67c3ead
John Hawkins <john@9seeds.com>
2013-01-25 11:17:50 -0800

    Added ticket/package add pages
    Added ticket/package add pages
    
    Signed-off-by: John Hawkins <john@9seeds.com>

--- views/admin/packages-add.php
+++ views/admin/packages-add.php
@@ -1,8 +1,65 @@
-pacakges add
+<div class="wrap">
+	<a href="http://9seeds.com/" target="_blank"><div id="seeds-icon"></div></a>
+	<h2><?php _e('Add Package', 'wpet'); ?></h2>
+<form method="post" action="">
+	<table class="form-table">
+		<tbody>
+			<tr class="form-field form-required">
+				<th scope="row"><?php _e('Package Name', 'wpet'); ?></th>
+				<td><input name="" type="text" id="" value=""></td>
+			</tr>
+			<tr class="form-field form-required">
+				<th scope="row"><?php _e('Description', 'wpet'); ?></th>
+				<td><textarea name="" type="text" id="" value=""></textarea></td>
+			</tr>
+		</tbody>
+	</table>
+	<h2><?php _e('Included Tickets', 'wpet'); ?></h2>
+	<table class="form-table">
+		<tbody>
+			<tr class="form-field form-required">
+				<th scope="row"><?php _e('Ticket Name', 'wpet'); ?></th>
+				<td><input name="" type="text" id="" value=""><br />
+				<a href=""><?php _e('remove', 'wpet'); ?></a></td>
+			</tr>
+		</tbody>
+	</table>
+	<h2><?php _e('On Sale Date', 'wpet'); ?></h2>
+	<table class="form-table">
+		<tbody>
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+// @TODO
+// Add date pickers
 ?>
+			<tr class="form-field form-required">
+				<th scope="row"><?php _e('Start Date', 'wpet'); ?></th>
+				<td><input name="" type="text" id="" value=""></td>
+			</tr>
+			<tr class="form-field form-required">
+				<th scope="row"><?php _e('End Date', 'wpet'); ?></th>
+				<td><input name="" type="text" id="" value=""></td>
+			</tr>
+		</tbody>
+	</table>
+	<h2><?php _e('Price', 'wpet'); ?></h2>
+	<table class="form-table">
+		<tbody>
+			<tr class="form-field form-required">
+				<th scope="row"><?php _e('Package Cost', 'wpet'); ?></th>
+				<td><input name="" type="text" id="" value=""></td>
+			</tr>
+		</tbody>
+	</table>
+	<h2><?php _e('Packages Available', 'wpet'); ?></h2>
+	<table class="form-table">
+		<tbody>
+			<tr class="form-field form-required">
+				<th scope="row"><?php _e('Quantity', 'wpet'); ?></th>
+				<td><input name="" type="text" id="" value=""></td>
+			</tr>
+		</tbody>
+	</table>
+	<p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary" value="<?php _e('Add Ticket', 'wpet'); ?>"></p>
+</form>
+
+</div>
--- views/admin/tickets-add.php
+++ views/admin/tickets-add.php
@@ -1,7 +1,28 @@
+<div class="wrap">
+	<a href="http://9seeds.com/" target="_blank"><div id="seeds-icon"></div></a>
+	<h2><?php _e('Add Ticket', 'wpet'); ?></h2>
+<form method="post" action="">
+	<table class="form-table">
+		<tbody>
+			<tr class="form-field form-required">
+				<th scope="row"><?php _e('Ticket Name', 'wpet'); ?></th>
+				<td><input name="" type="text" id="" value=""></td>
+			</tr>
+			<tr class="form-field form-required">
+				<th scope="row"><?php _e('Ticket Options', 'wpet'); ?></th>
+				<td>
+					<input type="checkbox" name="" value="1" checked="checked"> Name<br />
+					<input type="checkbox" name="" value="1" checked="checked"> Email<br />
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+// @TODO
+// auto-fill checkboxes with all available ticket options
 ?>
+
+				</td>
+			</tr>
+		</tbody>
+	</table>
+	<p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary" value="<?php _e('Add Ticket', 'wpet'); ?>"></p>
+</form>
+
+</div>
