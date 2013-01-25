b3d83a573b5d0d278dd6bda2e5127fcc2e2b533d
John Hawkins <john@9seeds.com>
2013-01-25 09:19:50 -0800

    event settings page ready
    event settings page ready
    
    Signed-off-by: John Hawkins <john@9seeds.com>

--- views/admin/settings-event.php
+++ views/admin/settings-event.php
@@ -12,5 +12,66 @@
 		<a href="settings-email.php?id=2" class="nav-tab">Email Settings</a>
 		<a href="settings-reset.php?id=2" class="nav-tab">Reset Settings</a>
 	</h3>
-
+	<?php
+// @TODO
+// Update form action to actually do something useful
+// Deal with nonces, referrer and hidden ID
+	?>
+	<form method="post" action="site-info.php?action=update-site">
+		<input type="hidden" id="_wpnonce" name="_wpnonce" value="27bb2390ea">
+		<input type="hidden" name="_wp_http_referer" value="/wp-admin/network/site-info.php?id=2">
+		<input type="hidden" name="id" value="2">
+		<table class="form-table">
+			<tbody>
+				<tr class="form-field form-required">
+					<th scope="row">Event Name</th>
+					<td><input name="" type="text" id="" value=""></td>
+				</tr>
+<?php
+// @TODO
+// Add date picker for event date
+?>
+				<tr class="form-field form-required">
+					<th scope="row">Event Date</th>
+					<td><input name="" type="text" id="" value=""></td>
+				</tr>
+				<tr class="form-field form-required">
+					<th scope="row">Organizer Name</th>
+					<td><input name="" type="text" id="" value=""></td>
+				</tr>
+				<tr class="form-field form-required">
+					<th scope="row">Organizer Email</th>
+					<td><input name="" type="text" id="" value=""></td>
+				</tr>
+				<tr class="form-field form-required">
+					<th scope="row">Maximum Attendance</th>
+					<td><input name="" type="text" id="" value=""></td>
+				</tr>
+				<tr class="form-field form-required">
+					<th scope="row">Event Status</th>
+					<td>
+						<select name="" id="">
+							<option value="">Registration is closed</option>
+							<option value="">Registration is open</option>
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<th scope="row">Display totals in form</th>
+					<td>
+						<label><input type="checkbox" name="" value="1" checked="checked"></label>
+					</td>
+				</tr>
+				<tr class="form-field form-required">
+					<th scope="row">Coming soon text</th>
+					<td><textarea name="" id="" value=""></textarea></td>
+				</tr>
+				<tr class="form-field form-required">
+					<th scope="row">Thank you page text</th>
+					<td><textarea name="" id="" value=""></textarea></td>
+				</tr>
+			</tbody>
+		</table>
+		<p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes"></p>
+	</form>
 </div>
