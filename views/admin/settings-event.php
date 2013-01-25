afc038e4b93ce0e277d3280eaddfdccbf6231996
John Hawkins <john@9seeds.com>
2013-01-25 10:14:10 -0800

    hold for pull
    hold for pull
    
    Signed-off-by: John Hawkins <john@9seeds.com>

--- views/admin/settings-event.php
+++ views/admin/settings-event.php
@@ -2,47 +2,49 @@
 // @TODO
 // Add date picker for event date
 ?>
-				<tr class="form-field form-required">
-					<th scope="row">Event Date</th>
-					<td><input name="" type="text" id="" value=""></td>
-				</tr>
-				<tr class="form-field form-required">
-					<th scope="row">Organizer Name</th>
-					<td><input name="" type="text" id="" value=""></td>
-				</tr>
-				<tr class="form-field form-required">
-					<th scope="row">Organizer Email</th>
-					<td><input name="" type="text" id="" value=""></td>
-				</tr>
-				<tr class="form-field form-required">
-					<th scope="row">Maximum Attendance</th>
-					<td><input name="" type="text" id="" value=""></td>
-				</tr>
-				<tr class="form-field form-required">
-					<th scope="row">Event Status</th>
-					<td>
-						<select name="" id="">
-							<option value="">Registration is closed</option>
-							<option value="">Registration is open</option>
-						</select>
-					</td>
-				</tr>
-				<tr>
-					<th scope="row">Display totals in form</th>
-					<td>
-						<label><input type="checkbox" name="" value="1" checked="checked"></label>
-					</td>
-				</tr>
-				<tr class="form-field form-required">
-					<th scope="row">Coming soon text</th>
-					<td><textarea name="" id="" value=""></textarea></td>
-				</tr>
-				<tr class="form-field form-required">
-					<th scope="row">Thank you page text</th>
-					<td><textarea name="" id="" value=""></textarea></td>
-				</tr>
-			</tbody>
-		</table>
-		<p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes"></p>
-	</form>
-</div>
+<form method="post" action="">
+	<table class="form-table">
+		<tbody>
+			<tr class="form-field form-required">
+				<th scope="row">Event Date</th>
+				<td><input name="" type="text" id="" value=""></td>
+			</tr>
+			<tr class="form-field form-required">
+				<th scope="row">Organizer Name</th>
+				<td><input name="" type="text" id="" value=""></td>
+			</tr>
+			<tr class="form-field form-required">
+				<th scope="row">Organizer Email</th>
+				<td><input name="" type="text" id="" value=""></td>
+			</tr>
+			<tr class="form-field form-required">
+				<th scope="row">Maximum Attendance</th>
+				<td><input name="" type="text" id="" value=""></td>
+			</tr>
+			<tr class="form-field form-required">
+				<th scope="row">Event Status</th>
+				<td>
+					<select name="" id="">
+						<option value="">Registration is closed</option>
+						<option value="">Registration is open</option>
+					</select>
+				</td>
+			</tr>
+			<tr>
+				<th scope="row">Display totals in form</th>
+				<td>
+					<label><input type="checkbox" name="" value="1" checked="checked"></label>
+				</td>
+			</tr>
+			<tr class="form-field form-required">
+				<th scope="row">Coming soon text</th>
+				<td><textarea name="" id="" value=""></textarea></td>
+			</tr>
+			<tr class="form-field form-required">
+				<th scope="row">Thank you page text</th>
+				<td><textarea name="" id="" value=""></textarea></td>
+			</tr>
+		</tbody>
+	</table>
+	<p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes"></p>
+</form>
