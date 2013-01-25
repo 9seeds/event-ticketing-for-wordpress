b08f78d36200abbfbb008b1f2389d3ab0adb536a
John Hawkins <john@9seeds.com>
2013-01-25 09:02:37 -0800

    Settings page tweaks
    Settings page tweaks
    
    Signed-off-by: John Hawkins <john@9seeds.com>

---
+++ views/admin/settings-event.php
@@ -0,0 +1,16 @@
+<div class="wrap">
+	<a href="http://9seeds.com/" target="_blank"><div id="seeds-icon"></div></a>
+	<h2><?php _e('Event Settings', 'wpet'); ?></h2>
+
+	<h3 class="nav-tab-wrapper">
+		<?php
+		// @TODO
+		// Replace id=2 with event ID
+		?>
+		<a href="settings-event.php?id=2" class="nav-tab nav-tab-active">Event Settings</a>
+		<a href="settings-payment.php?id=2" class="nav-tab">Payment Settings</a>
+		<a href="settings-email.php?id=2" class="nav-tab">Email Settings</a>
+		<a href="settings-reset.php?id=2" class="nav-tab">Reset Settings</a>
+	</h3>
+
+</div>
