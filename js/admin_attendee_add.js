b4e338ab2db00973b3070e2311adee4c17440585
Ben Lobaugh <ben@lobaugh.net>
2013-01-27 11:09:13 -0800

    Added js files

---
+++ js/admin_attendee_add.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function(){
+   
+    jQuery("#packages").change(function( package_id ) {
+	//console.log( jQuery(this).val() );
+	jQuery.post(
+	    ajaxurl, 
+	    {
+		action: "get_ticket_options_for_package", 
+		package_id : jQuery(this).val() 
+	    //nonce: nonce
+	    },
+	    function(response) {
+		console.log(response);
+		jQuery('#ticket_options').html(response);
+		
+	    }
+	         
+	    );
+   
+    });
+});
