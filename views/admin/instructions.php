fdaad6b9e17d8395de9fd3ffc3ffba53b0fcd749
John Hawkins <john@9seeds.com>
2013-01-27 13:04:43 -0800

    fixed some issues with labels and ids
    fixed some issues with labels and ids
    
    Signed-off-by: John Hawkins <john@9seeds.com>

--- views/admin/instructions.php
+++ views/admin/instructions.php
@@ -1,4 +1,4 @@
-<h2><?php _e('Instructions', 'wpet'); ?> <a href="<?php echo $data['new_url'] ?>" class="add-new-h2"><?php _e( 'Add New', 'wpet' ); ?></a></h2>
+<h2><?php _e('Instructions', 'wpet'); ?></h2>
 <h3><?php _e('Page Specific Help', 'wpet'); ?></h2>
 <p><?php _e('Just a reminder, while you are working inside WP Event Ticketing you can click the Help button in the top-right corner of the screen to receive page-specific help.', 'wpet'); ?></p>
 
