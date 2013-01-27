cb570024228ea319d41953f81b74504f1f953cf6
John Hawkins <john@9seeds.com>
2013-01-27 12:38:25 -0800

    Updated add/edit links on add/edit pages
    Updated add/edit links on add/edit pages
    
    Signed-off-by: John Hawkins <john@9seeds.com>

--- views/admin/packages-add.php
+++ views/admin/packages-add.php
@@ -1,19 +1,21 @@
-<div class="wrap">
-	<?php
-	echo $admin_page_icon;
-	$heading = empty( $_REQUEST['post'] ) ? __('Add Package', 'wpet') : __('Edit Package', 'wpet');
-	?>
-	<h2><?php echo $heading; ?> <?php if( isset($_GET['action'] ) && $_GET['action'] == 'edit' ) { echo '<a href="'. $data['edit_url'] .'" class="add-new-h2">'. __( 'Add New', 'wpet' ) .'</a>'; } ?></h2>
+<?php
+$heading = empty($_REQUEST['post']) ? __('Add Package', 'wpet') : __('Edit Package', 'wpet');
+?>
+<h2><?php echo $heading; ?> <?php
+if (isset($_GET['action']) && $_GET['action'] == 'edit') {
+	echo '<a href="' . $data['edit_url'] . '" class="add-new-h2">' . __('Add New', 'wpet') . '</a>';
+}
+?></h2>
 <form method="post" action="">
 	<table class="form-table">
 		<tbody>
 			<tr class="form-field form-required">
 				<th scope="row"><label for="package_name"><?php _e('Package Name', 'wpet'); ?></label></th>
-				<td><input name="options[package_name]" type="text" id="package_name" value="<?php echo empty( $data['package'] ) ? '' : $data['package']->post_title ?>"></td>
+				<td><input name="options[package_name]" type="text" id="package_name" value="<?php echo empty($data['package']) ? '' : $data['package']->post_title ?>"></td>
 			</tr>
 			<tr class="form-field form-required">
 				<th scope="row"><label for="description"><?php _e('Description', 'wpet'); ?></description></th>
-				<td><textarea name="options[description]" type="text" id="description"><?php echo empty( $data['package'] ) ? '' : $data['package']->post_content ?></textarea></td>
+				<td><textarea name="options[description]" type="text" id="description"><?php echo empty($data['package']) ? '' : $data['package']->post_content ?></textarea></td>
 			</tr>
 		</tbody>
 	</table>
@@ -24,14 +26,14 @@
 				<th scope="row"><label for="ticket_id"><?php _e('Ticket Name', 'wpet'); ?></label></th>
 				<td>
 					<?php
-						$ticket_id = empty( $data['package'] ) ? NULL : $data['package']->wpet_ticket_id;
-						echo WPET::getInstance()->tickets->selectMenu( 'options[ticket_id]', 'ticket_id', $ticket_id );
+					$ticket_id = empty($data['package']) ? NULL : $data['package']->wpet_ticket_id;
+					echo WPET::getInstance()->tickets->selectMenu('options[ticket_id]', 'ticket_id', $ticket_id);
 					?>
 				</td>
 			</tr>
 			<tr class="form-field form-required">
 				<th scope="row"><label for="ticket_quantity"><?php _e('Quantity', 'wpet'); ?></label></th>
-				<td><input name="options[ticket_quantity]" type="text" id="ticket_quantity" value="<?php echo empty( $data['package'] ) ? '' : $data['package']->wpet_ticket_quantity ?>"></td>
+				<td><input name="options[ticket_quantity]" type="text" id="ticket_quantity" value="<?php echo empty($data['package']) ? '' : $data['package']->wpet_ticket_quantity ?>"></td>
 			</tr>
 		</tbody>
 	</table>
@@ -40,11 +42,11 @@
 		<tbody>
 			<tr class="form-field form-required">
 				<th scope="row"><label for="start_date"><?php _e('Start Date', 'wpet'); ?></label></th>
-				<td><input name="options[start_date]" type="text" id="start_date" value="<?php echo empty( $data['package'] ) ? '' : $data['package']->wpet_start_date ?>"></td>
+				<td><input name="options[start_date]" type="text" id="start_date" value="<?php echo empty($data['package']) ? '' : $data['package']->wpet_start_date ?>"></td>
 			</tr>
 			<tr class="form-field form-required">
 				<th scope="row"><label for="end_date"><?php _e('End Date', 'wpet'); ?></label></th>
-				<td><input name="options[end_date]" type="text" id="end_date" value="<?php echo empty( $data['package'] ) ? '' : $data['package']->wpet_end_date ?>"></td>
+				<td><input name="options[end_date]" type="text" id="end_date" value="<?php echo empty($data['package']) ? '' : $data['package']->wpet_end_date ?>"></td>
 			</tr>
 		</tbody>
 	</table>
@@ -53,7 +55,7 @@
 		<tbody>
 			<tr class="form-field form-required">
 				<th scope="row"><label for="package_cost"><?php _e('Package Cost', 'wpet'); ?></label></th>
-				<td><input name="options[package_cost]" type="text" id="package_cost" value="<?php echo empty( $data['package'] ) ? '' : $data['package']->wpet_package_cost ?>"></td>
+				<td><input name="options[package_cost]" type="text" id="package_cost" value="<?php echo empty($data['package']) ? '' : $data['package']->wpet_package_cost ?>"></td>
 			</tr>
 		</tbody>
 	</table>
@@ -62,11 +64,10 @@
 		<tbody>
 			<tr class="form-field form-required">
 				<th scope="row"><label for="quantity"><?php _e('Quantity', 'wpet'); ?></label></th>
-				<td><input name="options[quantity]" type="text" id="quantity" value="<?php echo empty( $data['package'] ) ? '' : $data['package']->wpet_quantity ?>"></td>
+				<td><input name="options[quantity]" type="text" id="quantity" value="<?php echo empty($data['package']) ? '' : $data['package']->wpet_quantity ?>"></td>
 			</tr>
 		</tbody>
 	</table>
-	<?php echo $data['nonce'] ?>
+<?php echo $data['nonce'] ?>
 	<p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary" value="<?php _e('Add Package', 'wpet'); ?>"></p>
 </form>
-</div>
--- views/admin/ticket-options-add.php
+++ views/admin/ticket-options-add.php
@@ -1,62 +1,65 @@
-<div class="wrap">
-	<?php
-	echo $admin_page_icon;
-	$heading = empty( $_REQUEST['post'] ) ? __('Add Ticket Options', 'wpet') : __('Edit Ticket Options', 'wpet');
-	?>
-	<h2><?php echo $heading; ?> <?php if( isset($_GET['action'] ) && $_GET['action'] == 'edit' ) { echo '<a href="'. $data['edit_url'] .'" class="add-new-h2">'. __( 'Add New', 'wpet' ) .'</a>'; } ?></h2>
-	<form method="post" action="<?php echo $data['edit_url'] ?>">
+<?php
+$heading = empty($_REQUEST['post']) ? __('Add Ticket Option', 'wpet') : __('Edit Ticket Option', 'wpet');
+?>
+<h2><?php echo $heading; ?> <?php
+if (isset($_GET['action']) && $_GET['action'] == 'edit') {
+	echo '<a href="' . $data['edit_url'] . '" class="add-new-h2">' . __('Add New', 'wpet') . '</a>';
+}
+?></h2>
+<form method="post" action="<?php echo $data['edit_url'] ?>">
 	<table class="form-table">
 		<tbody>
 			<tr class="form-field form-required">
 				<th scope="row"><label for="display_name"><?php _e('Display Name', 'wpet'); ?></label></th>
-				<td><input name="options[display_name]" type="text" id="display_name" value="<?php echo isset( $data['option'] ) ? $data['option']->post_title : '' ?>"></td>
+				<td><input name="options[display_name]" type="text" id="display_name" value="<?php echo isset($data['option']) ? $data['option']->post_title : '' ?>"></td>
 			</tr>
 			<tr class="form-field form-required">
 				<th scope="row"><label for="option_type"><?php _e('Option Type', 'wpet'); ?></label></th>
 				<td>
 					<select name="options[option_type]" id="option_type">
-					<?php
-					$value = empty( $data['option'] ) ? '' : $data['option']->wpet_type;
-					$options = array(
-						'text'        => __('Text Input', 'wpet'),
-						'dropdown'    => __('Dropdown', 'wpet'),
-						'multiselect' => __('Multi Select', 'wpet'),
-					);
-					foreach ( $options as $index => $label ) {
-						$selected = $index == $value ? ' selected="selected"' : '';
-						echo "<option value='{$index}'{$selected}>{$label}</option>\n";
-					}
-					?>
+						<?php
+						$value = empty($data['option']) ? '' : $data['option']->wpet_type;
+						$options = array(
+							'text' => __('Text Input', 'wpet'),
+							'dropdown' => __('Dropdown', 'wpet'),
+							'multiselect' => __('Multi Select', 'wpet'),
+						);
+						foreach ($options as $index => $label) {
+							$selected = $index == $value ? ' selected="selected"' : '';
+							echo "<option value='{$index}'{$selected}>{$label}</option>\n";
+						}
+						?>
 					</select>
 				</td>
 			</tr>
 		</tbody>
 		<?php
 		$extra_style = 'style="display: none;" ';
-		if ( ! empty( $data['option'] ) && in_array( $data['option']->wpet_type, array( 'dropdown', 'multiselect' ) ) ) {
+		if (!empty($data['option']) && in_array($data['option']->wpet_type, array('dropdown', 'multiselect'))) {
 			$extra_style = '';
 		}
 		?>
 		<tbody <?php echo $extra_style ?>id="option_values">
 			<?php
 			$count = 0;
-			$values = empty( $data['option'] ) ? array() : $data['option']->wpet_values;
-			if ( empty( $values ) )
+			$values = empty($data['option']) ? array() : $data['option']->wpet_values;
+			if (empty($values))
 				$values[] = '';
-			foreach ( $values  as $value ):
+			foreach ($values as $value):
 				$delete = $count ? '<span class="wpet-delete">&nbsp;<a class="option-delete">X</a>' : '';
-			?>
-			<tr class="form-field">
-				<th scope="row"><label for="option_value[<?php echo $count; ?>]"><?php _e('Option Value', 'wpet'); ?></label></th>
-				<td>
-					<input type="text" name="options[option_value][]" id="option_value[<?php echo $count; ?>]" class="option-value" value="<?php echo $value ?>" /><?php echo $delete ?>
-				</td>
-			</tr>
-			<?php $count++; endforeach; ?>
+				?>
+				<tr class="form-field">
+					<th scope="row"><label for="option_value[<?php echo $count; ?>]"><?php _e('Option Value', 'wpet'); ?></label></th>
+					<td>
+						<input type="text" name="options[option_value][]" id="option_value[<?php echo $count; ?>]" class="option-value" value="<?php echo $value ?>" /><?php echo $delete ?>
+					</td>
+				</tr>
+	<?php $count++;
+endforeach; ?>
 			<tr class="form-field" id="add-another">
-	 			<th></th>
+				<th></th>
 				<td>
-					<a id="add-ticket-option"><?php _e( 'Add Another Value', 'wpet' ) ?></a>
+					<a id="add-ticket-option"><?php _e('Add Another Value', 'wpet') ?></a>
 				</td>
 			</tr>
 			<tr class="form-field" style="display: none;" id="new-value">
@@ -69,8 +72,7 @@
 	</table>
 	<?php
 	echo $data['nonce'];
-	$button_label = empty( $_REQUEST['post'] ) ? __( 'Add Ticket Option', 'wpet' ) : __( 'Edit Ticket Option', 'wpet' );
+	$button_label = empty($_REQUEST['post']) ? __('Add Ticket Option', 'wpet') : __('Edit Ticket Option', 'wpet');
 	?>
 	<p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary" value="<?php echo $button_label; ?>"></p>
-	</form>
-</div>
+</form>
