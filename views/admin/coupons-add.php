6052b176dbfba9ef180e7da0e8362d60757ca5b4
Ben Lobaugh <ben@lobaugh.net>
2013-01-31 22:17:16 -0800

    Add coupons now no borky

--- views/admin/coupons-add.php
+++ views/admin/coupons-add.php
@@ -12,30 +12,30 @@ if (isset($_GET['action']) && $_GET['action'] == 'edit') {
 		<tbody>
 			<tr class="form-field form-required">
 				<th scope="row"><label for="coupon_code"><?php _e('Coupon Code', 'wpet'); ?></lable></th>
-				<td><input name="options[coupon-code]" id="coupon_code" type="text" value="<?php echo $data['coupon']->post_name; ?>"></td>
+				<td><input name="options[coupon-code]" id="coupon_code" type="text" value="<?php echo @$data['coupon']->post_name; ?>"></td>
 			</tr>
 			<tr class="form-field form-required">
 				<th scope="row"><label for="package_id"><?php _e('Package', 'wpet'); ?></label></th>
 				<td>
-					<?php echo WPET::getInstance()->packages->selectMenu('options[package_id]', 'package_id', $data['coupon']->wpet_package_id ); ?>
+					<?php echo WPET::getInstance()->packages->selectMenu('options[package_id]', 'package_id', @$data['coupon']->wpet_package_id ); ?>
 				</td>
 			</tr>
 			<tr class="form-field form-required">
 				<th scope="row"><label for="type"><?php _e('Type', 'wpet'); ?></label></th>
 				<td>
 					<select name="options[type]" id="type">
-						<option value="flat-rate" <?php selected( 'flat-rate', $data['coupon']->wpet_type, true ); ?>><?php _e('Flat Rate', 'wpet'); ?></option>
-						<option value="percentage" <?php selected( 'percentage', $data['coupon']->wpet_type, true ); ?>><?php _e('Percentage', 'wpet'); ?></option>
+						<option value="flat-rate" <?php selected( 'flat-rate', @$data['coupon']->wpet_type, true ); ?>><?php _e('Flat Rate', 'wpet'); ?></option>
+						<option value="percentage" <?php selected( 'percentage', @$data['coupon']->wpet_type, true ); ?>><?php _e('Percentage', 'wpet'); ?></option>
 					</select>
 				</td>
 			</tr>
 			<tr class="form-field form-required">
 				<th scope="row"><label for="amount"><?php _e('Amount', 'wpet'); ?></label></th>
-				<td><input name="options[amount]" id="amount" type="text" value="<?php echo $data['coupon']->wpet_amount; ?>"></td>
+				<td><input name="options[amount]" id="amount" type="text" value="<?php echo @$data['coupon']->wpet_amount; ?>"></td>
 			</tr>
 			<tr class="form-field form-required">
 				<th scope="row"><label for="quantity"><?php _e('Quantity', 'wpet'); ?></label></th>
-				<td><input name="options[quantity]" id="quantity" type="text" value="<?php echo $data['coupon']->wpet_quantity; ?>"></td>
+				<td><input name="options[quantity]" id="quantity" type="text" value="<?php echo @$data['coupon']->wpet_quantity; ?>"></td>
 			</tr>
 
 		</tbody>
