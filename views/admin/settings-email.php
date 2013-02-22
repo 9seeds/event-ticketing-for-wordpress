977100b863c40cf8d952660fdf20b8e0e2481f60
Ben Lobaugh <ben@lobaugh.net>
2013-02-21 20:34:47 -0800

    Removed email from settings tab

--- views/admin/settings-email.php
+++ views/admin/settings-email.php
@@ -1,14 +1,14 @@
 <h2><?php _e('Registration Email', 'wpet'); ?></h2>
 <table class="form-table">
 	<tbody>
-		<tr class="form-field form-required">
+		<!--<tr class="form-field form-required">
 			<th scope="row"><label for="options[from_name]"><?php _e('From Name', 'wpet'); ?></label></th>
 			<td><input name="options[from_name]" type="text" id="options[from_name]" value="<?php esc_attr_e($data['from_name']) ?>"></td>
 		</tr>
 		<tr class="form-field form-required">
 			<th scope="row"><label for="options[from_email]"><?php _e('From Email', 'wpet'); ?></label></th>
 			<td><input name="options[from_email]" type="text" id="options[from_email]" value="<?php esc_attr_e($data['from_email']) ?>"></td>
-		</tr>
+		</tr>-->
 		<tr class="form-field form-required">
 			<th scope="row"><label for="options[subject]"><?php _e('Subject', 'wpet'); ?></label></th>
 			<td><input name="options[subject]" type="text" id="options[subject]" value="<?php esc_attr_e($data['subject']) ?>"></td>
