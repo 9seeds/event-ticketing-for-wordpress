3a8302ac1979f1c75b90892a3751ec21897c1e7f
Justin Foell <justin@9seeds.com>
2013-04-22 23:31:12 -0500

    Added check for no coupon code

--- js/admin_coupons.js
+++ js/admin_coupons.js
@@ -2,7 +2,13 @@ jQuery(document).ready(function($) {
 
 	$('#wpet_admin_coupons_add').submit(function() {
 		//do validation
-		if ( ! $.isNumeric( $( '#amount' ).val() ) ) {
+		if ( jQuery.trim( $( '#coupon_code' ).val() ) == '' ) {
+				alert( wpet_coupons_add.name_required );
+				return false;
+		}
+
+
+		if ( ! jQuery.isNumeric( $( '#amount' ).val() ) ) {
 				alert( wpet_coupons_add.amount_not_numeric );
 				return false;
 		}
