6630797c9bee5fa6956136d5eb2352fa11e8fadf
John Hawkins <john@9seeds.com>
2013-05-06 19:43:33 -0700

    fixed issue with square bracket not allowing js selection
    fixed issue with square bracket not allowing js selection

--- js/admin_tickets.js
+++ js/admin_tickets.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($) {
 	$('#wpet_admin_ticket_add').submit(function() {
 		//do validation
-		if ( jQuery.trim( $( '#options[ticket-name]' ).val() ) == '' ) {
+		if ( jQuery.trim( $( '#options\\[ticket-name\\]' ).val() ) == '' ) {
 				alert( wpet_tickets_add.name_required );
 				return false;
 		}
