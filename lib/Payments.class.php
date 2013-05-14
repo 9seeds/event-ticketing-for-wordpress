72caad6cddc25dba45a59a76d24ea5f57b0ee258
Ben Lobaugh <ben@lobaugh.net>
2013-05-13 19:29:45 -0700

    Fixed displayed total on cart

--- lib/Payments.class.php
+++ lib/Payments.class.php
@@ -449,9 +449,11 @@ class WPET_Payments extends WPET_Module {
 		    'package_cost' => $package->wpet_package_cost,
 		    'quantity' => $quantity,
 		);
-		$cart['total'] += $package->wpet_package_cost * $quantity;
+		//$cart['total'] += $package->wpet_package_cost * $quantity;
 	    }
 	}
+	
+	$cart['total'] = $this->mPayment->wpet_total;
 	return $cart;
     }
 
