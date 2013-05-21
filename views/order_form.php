9a646d277b1b7d0dd755d4d0db46717eafbf911a
Ben Lobaugh <ben@lobaugh.net>
2013-05-20 22:49:51 -0700

    Packages should now only show on the form if they are within the package sell dates

--- views/order_form.php
+++ views/order_form.php
@@ -1,6 +1,14 @@
 <div id="eventTicketing">
 
-    <?php if( 'closed' == $data['event']->wpet_event_status ) { 
+    
+    <?php 
+    
+   // $working_event = WPET::getInstance()->events->getWorkingEvent();
+   // echo "<p>Now: " . time() . "<br/>Sta: " . var_dump( $working_event) . "</p>";
+    if( 'closed' == $data['event']->wpet_event_status 
+	     /* || time() < strtotime( $working_event->wpet_start_date )
+	      || time() > strtotime( $working_event->wpet_end_date )*/
+	    ) { 
     
     
     
@@ -31,6 +39,16 @@
 					<?php _e( 'Quantity', 'wpet' ); ?></th>
 				</tr>
 				<?php foreach( $data['rows'] as $row ): ?>
+				<?php
+				if( time() < strtotime($row->wpet_start_date)
+				    || time() > strtotime($row->wpet_end_date)
+				  ) {
+				    // This package is not open for purchasing yet
+				    
+				    continue;
+				}
+				
+				?>
 				<tr class="package_row">
 
 					<td>
