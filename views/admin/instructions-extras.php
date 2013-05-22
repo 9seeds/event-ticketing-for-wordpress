bdc4dcdd25a25808b54f5fa0d57d6ef3427862a6
John Hawkins <john@9seeds.com>
2013-05-21 21:52:49 -0700

    i18n cleanup (thanks, otto)
    i18n cleanup (thanks, otto)

--- views/admin/instructions-extras.php
+++ views/admin/instructions-extras.php
@@ -1,14 +1,14 @@
-<h3><?php _e( '<strong>Reporting</strong>', 'wpet' ); ?></h3>
-<p><?php _e( 'The <a href="admin.php?page=wpet_reports">Reporting</a> page gives you a snap shot of the packages and ticket types sold, coupons used and a graph displaying the number of tickets used and still available. The Summary Report makes it easy to get a count of your attendees based on the data provided. Very handy for getting a count of how many t-shirts you need to order in each size.', 'wpet' ); ?></p>
+<h3><?php _e( 'Reporting', 'wpet' ); ?></h3>
+<p><?php sprintf( _e( 'The %s page gives you a snap shot of the packages and ticket types sold, coupons used and a graph displaying the number of tickets used and still available. The Summary Report makes it easy to get a count of your attendees based on the data provided. Very handy for getting a count of how many t-shirts you need to order in each size.', 'wpet' ), '<a href="admin.php?page=wpet_reports">Reporting</a>' ); ?></p>
 
-<h3><?php _e( '<strong>Create Coupons</strong>', 'wpet' ); ?></h3>
-<p><?php _e( 'On the <a href="admin.php?page=wpet_coupons">Coupons</a> page you can create an easy way to give discounts on tickets. Create a ticket code, set a flat-rate or percentage discount and select the number of times it can be used. This is handy for giving your speakers free entry to the event, but having them register so they are included in the attendee list and receive email notifications you might send.', 'wpet' ); ?></p>
+<h3><?php _e( 'Create Coupons', 'wpet' ); ?></h3>
+<p><?php sprintf( _e( 'On the %s page you can create an easy way to give discounts on tickets. Create a ticket code, set a flat-rate or percentage discount and select the number of times it can be used. This is handy for giving your speakers free entry to the event, but having them register so they are included in the attendee list and receive email notifications you might send.', 'wpet' ), '<a href="admin.php?page=wpet_coupons">Coupons</a>' ); ?></p>
 
-<h3><?php _e( '<strong>Send Emails to Attendees</strong>', 'wpet' ); ?></h3>
-<p><?php _e( 'The <a href="admin.php?page=wpet_notifications">Notify Attendees</a> page will let you send an email to everybody on the Attendee list. A copy of the email gets sent to the admin automatically. A history of the messages sent is stored and displayed at the bottom of the page.', 'wpet' ); ?></p>
+<h3><?php _e( 'Send Emails to Attendees', 'wpet' ); ?></h3>
+<p><?php sprintf( _e( 'The %s page will let you send an email to everybody on the Attendee list. A copy of the email gets sent to the admin automatically. A history of the messages sent is stored and displayed at the bottom of the page.', 'wpet' ), '<a href="admin.php?page=wpet_notifications">Notify Attendees</a>' ); ?></p>
 
-<h3><?php _e( '<strong>Create Tickets Manually</strong>', 'wpet' ); ?></h3>
-<p><?php _e( 'You may find it necessary to create a ticket for somebody manually. Use the <a href="admin.php?page=wpet_attendees&action=new">Add Attendee</a> page to create a ticket, as an example, for somebody who may have paid cash.', 'wpet' ); ?></p>
+<h3><?php _e( 'Create Tickets Manually', 'wpet' ); ?></h3>
+<p><?php sprintf( _e( 'You may find it necessary to create a ticket for somebody manually. Use the %s page to create a ticket, as an example, for somebody who may have paid cash.', 'wpet' ), '<a href="admin.php?page=wpet_attendees&action=new">Add Attendee</a>' ); ?></p>
 
-<h3><?php _e( '<strong>Export Attendee List</strong>', 'wpet' ); ?></h3>
+<h3><?php _e( 'Export Attendee List', 'wpet' ); ?></h3>
 <p><?php _e( 'Also on the Attendee page you have the option to export a list of all the attendees. This creates a CSV of all attendees and the data they\'ve provided.', 'wpet' ); ?></p>
