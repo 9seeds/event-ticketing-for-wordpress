bfe0075755c11cfbf546b0340788fdd99a72ceaf
John Hawkins <john@9seeds.com>
2013-06-10 20:50:40 -0700

    Wrapped up JS error checking on notify attendees page
    Wrapped up JS error checking on notify attendees page

--- js/admin_notifications.js
+++ js/admin_notifications.js
@@ -2,8 +2,9 @@ jQuery(document).ready(function($) {
 
 	$('#wpet_admin_notification_add').submit(function() {
 		//do validation
-		if ( ! ( $( '#all-attendees' ).is(':checked') || 
-			$( '#attendees-have-info' ).is(':checked') ) ) {
+		if ( ! ( $( '#all-attendees' ).is(':checked' ) || 
+			$( '#attendees-have-info' ).is(':checked' ) ||
+			$( '#attendees-no-info' ).is(':checked' ) ) ) {
             alert( wpet_notifications_add.send_to_required );
             return false;
         }
