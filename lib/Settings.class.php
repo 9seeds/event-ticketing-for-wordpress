75a930ac3a7f59ba84cbeaf48e75db84ae6958d7
Justin Foell <justin@9seeds.com>
2013-06-17 20:59:46 -0600

    Comment out reset code Add event ID to payment meta

--- lib/Settings.class.php
+++ lib/Settings.class.php
@@ -294,19 +294,18 @@ class WPET_Settings extends WPET_Module {
 		unset( $options['event_status'] );
 
 		//@TODO do the resets here
-		
+
+		/*
 		$reset = array();
 		foreach( $options['reset'] AS $k => $v ) {
 		    $reset[] = $k;
 		}
-		/**
-		 * @todo Remove individual elements
-		 */
 		require_once WPET_PLUGIN_DIR . 'lib/Installer.class.php';
 		$installer = new WPET_Installer();
 		$installer->reset( $reset );
 		unset( $options['reset'] );
-
+		*/
+		
 		foreach ( $options as $key => $value ) {
 			$this->{$key} = stripslashes( $value );
 		}
