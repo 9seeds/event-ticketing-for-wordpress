5872cbcab173fc8ae4d76dfb7f372ac0a4ad1d33
Josh Harrison <josh@picklewagon.com>
2013-06-17 20:54:54 -0600

    validate the notification email body even when there is not co

--- js/admin_notifications.js
+++ js/admin_notifications.js
@@ -3,20 +3,20 @@ jQuery(document).ready(function($) {
 	$('#wpet_admin_notification_add').submit(function() {
 		//do validation
 		if ( ! ( $( '#all-attendees' ).is(':checked' ) || 
-			$( '#attendees-have-info' ).is(':checked' ) ||
-			$( '#attendees-no-info' ).is(':checked' ) ) ) {
+			    $( '#attendees-have-info' ).is(':checked' ) ||
+			    $( '#attendees-no-info' ).is(':checked' ) ) ) {
             alert( wpet_notifications_add.send_to_required );
             return false;
         }
 
-		if ( jQuery.trim( $( '#options\\[subject\\]' ).val() ) == '' ) {
-				alert( wpet_notifications_add.subject_required );
-				return false;
+		if ( $.trim( $( '#options\\[subject\\]' ).val() ) == '' ) {
+			alert( wpet_notifications_add.subject_required );
+			return false;
 		}
 
-		if ( jQuery.trim( $( '#options\\[email_body\\]' ).val() ) == '' ) {
-				alert( wpet_notifications_add.body_required );
-				return false;
+		if ( tinyMCE.activeEditor.getContent() == '' ) {
+			alert( wpet_notifications_add.body_required );
+			return false;
 		}
 
 		return true;
