b426a664b62d3cb7c658e2ee8860bca72ad9caa3
Ben Lobaugh <ben@lobaugh.net>
2013-07-08 20:49:04 -0700

    Making things pretty

--- lib/WPET.class.php
+++ lib/WPET.class.php
@@ -288,7 +288,10 @@ class WPET {
 			if(file_exists( ABSPATH . '/WPET_DEV')) {
 			    // If we are in dev mode show the git hash
 			    exec('cd ' . WPET_PLUGIN_DIR . ' && git rev-parse --verify HEAD 2> /dev/null', $output);
-			    $links[] = 'Githash: ' . $output[0];
+			    $hash = $output[0];
+			    exec('cd ' . WPET_PLUGIN_DIR . ' && git rev-parse --abbrev-ref HEAD 2> /dev/null', $output2);
+			    $branch = $output2[0];
+			    $links[] = '<b>Git</b> (<span style="color:red">branch</span>/<span style="color:green">hash</span>): <b style="color:red">' . $branch . '</b>/<span style="color:green">' . $hash . '</span>';
 			}
 		}
 
