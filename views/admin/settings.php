440674e195d5c6d314de455f7676edf84618e790
John Hawkins <john@9seeds.com>
2013-07-21 17:55:25 -0700

    Added tabs to settings page
    Added tabs to settings page

--- views/admin/settings.php
+++ views/admin/settings.php
@@ -1,34 +1,46 @@
 <h2><?php _e('Event Settings', 'wpet'); ?></h2>
+<?php settings_errors(); ?>
+
+<?php
+	$active_tab = isset( $_GET[ 'tab' ] ) ? $_GET[ 'tab' ] : 'event';
+?>
+
+<h2 class="nav-tab-wrapper">
+	<?php
+	foreach ($data['tabs'] as $tab_id => $tab) {
+		?>
+		<a href="?page=wpet_settings&tab=<?php echo $tab_id; ?>" class="nav-tab <?php echo $active_tab == $tab_id ? 'nav-tab-active' : ''; ?>"><?php echo $tab; ?></a>
+		<?php
+	}
+	?>
+</h2>
+
+<?php
+foreach ( $data['settings'] as $tab_id => $settings ) {
+
+	if ( !in_array( $tab_id, array_keys($data['tabs'] ) ) )
+		continue;
+	echo '<div>';
+	if( $tab_id == $active_tab ) {
+		echo "<div id='tab-{$tab_id}'>";
+
+		foreach ($settings AS $set) {
+			echo $set['text'];
+		}
+	}
+	echo '</div>';
+}
+?>
+
 
 <form action="" method="post" id="settings_form">
-	<div id="tabs">
-		<ul>
-			<?php
-			foreach ($data['tabs'] as $tab_id => $tab) {
-				echo "<li><a href='#tab-{$tab_id}'>{$tab}</a></li>\n";
-			}
-			?>
-		</ul>
 
-		<?php
-		foreach ($data['settings'] as $tab_id => $settings) {
+	<?php settings_fields( 'sandbox_theme_display_options' ); ?>
+	<?php do_settings_sections( 'sandbox_theme_display_options' ); ?>	
+	
+	<?php settings_fields( 'sandbox_theme_social_options' ); ?>
+	<?php do_settings_sections( 'sandbox_theme_social_options' ); ?>	
 
-			if (!in_array($tab_id, array_keys($data['tabs'])))
-				continue;
-
-			echo "<div id='tab-{$tab_id}'>";
-
-			foreach ($settings AS $set) {
-//		    echo "<h2>{$set['title']}</h2>";
-				echo $set['text'];
-			}
-
-			echo "</div>\n";
-		}
-		?>
-	</div><!-- #tabs -->
-	<p class="submit">
-<?php echo $data['nonce'] ?>
-		<input type="submit" name="submit" id="submit" class="button button-primary" value="<?php _e('Save All Settings', 'wpet'); ?>" />
-	</p>
+	<?php submit_button(); ?>
+	
 </form>
