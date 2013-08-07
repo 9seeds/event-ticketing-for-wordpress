66529334acfe11bb4184dbf82016131c91ffa0bc
John Hawkins <john@9seeds.com>
2013-08-06 23:57:17 -0700

    Reordered live/sandbox settings on payment tab. Also included links to walk-through videos for each.

--- views/admin/gateway-paypal-express.php
+++ views/admin/gateway-paypal-express.php
@@ -15,24 +15,8 @@
 
 		</tbody>
 	</table>
-	<h2><?php _e( 'Sandbox Settings', 'wpet' ); ?></h2>
-	<table class="form-table">
-		<tbody>
-			<tr class="form-field form-required">
-				<th scope="row"><label for="options[paypal_sandbox_api_username]"><?php _e('API Username', 'wpet'); ?></label></th>
-				<td><input name="options[paypal_sandbox_api_username]" type="text" id="options[paypal_sandbox_api_username]" value="<?php echo $data['paypal_sandbox_api_username'] ?>"></td>
-			</tr>
-			<tr class="form-field form-required">
-				<th scope="row"><label for="options[paypal_sandbox_api_password]"><?php _e('API Password', 'wpet'); ?></label></th>
-				<td><input name="options[paypal_sandbox_api_password]" type="password" id="options[paypal_sandbox_api_password]" value="<?php echo $data['paypal_sandbox_api_password'] ?>"></td>
-			</tr>
-			<tr class="form-field form-required">
-				<th scope="row"><label for="options[paypal_sandbox_api_signature]"><?php _e('API Signature', 'wpet'); ?></label></th>
-				<td><input name="options[paypal_sandbox_api_signature]" type="text" id="options[paypal_sandbox_api_signature]" value="<?php echo $data['paypal_sandbox_api_signature'] ?>"></td>
-			</tr>
-		</tbody>
-	</table>
 	<h2><?php _e( 'Live Settings', 'wpet' ); ?></h2>
+	<p><?php echo sprintf( __( 'Need help setting up your PayPal API Credentials? %sWatch Video%s', 'wpet'), '<a href="https://vimeo.com/71790459" target="_blank">', '</a>' ); ?>
 	<table class="form-table">
 		<tbody>
 			<tr class="form-field form-required">
@@ -46,6 +30,24 @@
 			<tr class="form-field form-required">
 				<th scope="row"><label for="options[paypal_live_api_signature]"><?php _e('API Signature', 'wpet'); ?></label></th>
 				<td><input name="options[paypal_live_api_signature]" type="text" id="options[paypal_live_api_signature]" value="<?php echo $data['paypal_live_api_signature'] ?>"></td>
+			</tr>
+		</tbody>
+	</table>
+	<h2><?php _e( 'Sandbox Settings', 'wpet' ); ?></h2>
+	<p><?php echo sprintf( __( 'Need help setting up your PayPal Sandbox Credentials? %sWatch Video%s', 'wpet'), '<a href="https://vimeo.com/71799044" target="_blank">', '</a>' ); ?>
+	<table class="form-table">
+		<tbody>
+			<tr class="form-field form-required">
+				<th scope="row"><label for="options[paypal_sandbox_api_username]"><?php _e('API Username', 'wpet'); ?></label></th>
+				<td><input name="options[paypal_sandbox_api_username]" type="text" id="options[paypal_sandbox_api_username]" value="<?php echo $data['paypal_sandbox_api_username'] ?>"></td>
+			</tr>
+			<tr class="form-field form-required">
+				<th scope="row"><label for="options[paypal_sandbox_api_password]"><?php _e('API Password', 'wpet'); ?></label></th>
+				<td><input name="options[paypal_sandbox_api_password]" type="password" id="options[paypal_sandbox_api_password]" value="<?php echo $data['paypal_sandbox_api_password'] ?>"></td>
+			</tr>
+			<tr class="form-field form-required">
+				<th scope="row"><label for="options[paypal_sandbox_api_signature]"><?php _e('API Signature', 'wpet'); ?></label></th>
+				<td><input name="options[paypal_sandbox_api_signature]" type="text" id="options[paypal_sandbox_api_signature]" value="<?php echo $data['paypal_sandbox_api_signature'] ?>"></td>
 			</tr>
 		</tbody>
 	</table>
